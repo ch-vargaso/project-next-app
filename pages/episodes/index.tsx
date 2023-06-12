672157f9d09a3bb6fc2ea6da8a9c08033926b05a
Christian Vargas <vazhara@gmail.com>
2023-06-12 15:35:00 +0200

    episodes fetch made using graphql....

--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -1,16 +1,57 @@
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import React from 'react'
 import styles from '../../styles/Episodes.module.css'
+import { gql, useQuery } from '@apollo/client'
+import GraphqlClientMainURL from '../../graphql/graphql-client';
 
+const GET_EPISODES = gql`
+    query getEpisodes($page: Int, $filter: FilterEpisode){
+    episodes(page:$page,
+            filter: $filter){
+            info{
+            count
+            pages
+            next
+            prev
+            }
+            results{
+            id
+            name
+            air_date
+            characters{
+                id
+                name
+                image}
+            }
+            }
+    }
+`;
 
 type Props = {
-    result: any
+    data: {
+        episodes: {
+            results: queryEpisode[]
+        }
+    }
+}
+
+type queryEpisode = {
+    id: string,
+    name: string
+    characters: Character[]
 }
 
 const episodes = (props: Props) => {
-    const { result } = props
-    const episodes = result.results
-    console.log('result :>> ', result);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { data, loading, error } = useQuery(GET_EPISODES);
+    console.log('data :>> ', data);
+    if (loading) {
+        return <p>loading...</p>
+    }
+    if (error) {
+        return <p>{error.message}</p>
+    }
+
     return (
         <div>
             <h1 className={styles.title} >Episodes</h1>
@@ -19,76 +60,88 @@ const episodes = (props: Props) => {
                     <tr>
                         <th>#</th>
                         <th>Name</th>
-                        <th>Fearuted Characters</th>
+                        <th>Featured Characters</th>
                     </tr>
-                    {episodes && episodes.map((episode: Episode) => {
-                        const characters = episode.characters
+                    {data.episodes.results.map((episode: queryEpisode) => {
                         return (
-                            <tr key={episode.id}>
+                            <tr key={episode.id} >
                                 <th>{episode.id}</th>
-                                <td>{episode.name}</td>
-                                {/* <td>{episode.created}</td> */}
-                                <td>
-
-                                </td>
+                                <th>{episode.name}</th>
+                                <th>
+                                    {episode.characters.map((character: Character) => {
+                                        return <img key={character.id} src={character.image} width={50} height={50} />
+                                    })}
+                                </th>
                             </tr>
                         )
-
- 
                     })}
-                    <tr>
-                        <th>hola</th>
-                        <td>kmkmkm</td>
-                        <td>kokok
-                            {/* <div>
-                            <img src="" alt="" />
-                        </div> */}
-                            {/* aquí hago la lista de imagenes pequeñas... */}
-                        </td>
-
-
-                    </tr>
-
-                    <tr>
-
-                    </tr>
-
                 </tbody>
             </table>
         </div>
 
     )
-}
+};
 
 export default episodes
 
 
-// here is the backend... 
+// ###################  BACK END ###################
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const {
+        query: { page },
+    } = context
 
-
-
-export const getServerSideProps: GetServerSideProps<{ result: Episode }> = async () => {
-    try {
-        const response = await fetch("https://rickandmortyapi.com/api/episode");
-        const result = await response.json();
-        return { props: { result: result } }
-    } catch (error) {
-        console.log('failed to fetch:>> ', error);
-        return { props: { result: null } }
+    const { data } = await GraphqlClientMainURL.query<Promise<Episodes>>({
+        query: GET_EPISODES,
+        variables: { page: + page! || 1 },
+    })
+    return {
+        props: { data },
     }
 }
 
-// export const getStaticPaths: GetStaticPaths = async (context) => {
-//     console.log('context :>> ', context);
-//     const urls = episodes
-//     const paths = []
-//     for (let 0 =; i <= episodes.length; i++) {
-//         const url = i.toString()
-//         const path = ``
-//     }
-// }
 
-// export const getStaticProps: GetStaticProps<{ result: Character }> = async () => {
-    
-// }
+
+
+
+//  this is the other fetch learned from Emily without using apollo client 
+
+    // const [data, setData] = useState();
+
+    // const getData = async () => {
+
+    //     try {
+    //         const response = await fetch('https://rickandmortyapi.com/graphql', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({
+    //                 query:
+    //                     `query {
+    //                     episodes{
+    //                         results{
+    //                         id
+    //                         name
+    //                         characters{
+    //                             id
+    //                             image
+    //                                 }
+    //                             }
+    //                         }
+    //                     }`
+    //             })
+
+    //         });
+    //         const result = await response.json()
+    //         console.log('episodes result :>> ', result)
+    //         setData(result)
+    //     } catch (error) {
+    //         console.log('error :>> ', error);
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     getData()
+    // }, [])
